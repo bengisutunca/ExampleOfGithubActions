085fa8175425073c154da7744d5a32daee6346ba
Bengisu Tunca <bengitunca@gmail.com>
2024-06-18 16:36:51 +0300

    * initial commit

---
+++ ExampleOfGithubActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleOfGithubActions
+//
+//  Created by Bengi Tunca on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
